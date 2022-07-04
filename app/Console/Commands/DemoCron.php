9f8d787611f0b7f9edc6f96b6a65f1468dabad2d
E8-Labs <nahomnega23@gmail.com>
2022-07-04 12:50:48 +0500

    Crone Job configured

--- app/Console/Commands/DemoCron.php
+++ app/Console/Commands/DemoCron.php
@@ -4,6 +4,14 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\ReportController;
+use App\Models\Auth\User;
+use App\Models\Auth\AccountStatus;
+use App\Models\Auth\UserType;
+
+use Carbon\Carbon;
+
 class DemoCron extends Command
 {
     /**
@@ -28,6 +36,148 @@ class DemoCron extends Command
     public function handle()
     {
         \Log::info("Cron is working fine!");
+        $users = User::get();
+        // User::whereNotNull('chekrreportid')->where('chekrreportid', '!=', '')
+                    // ->whereNotNull('chekrstatus ')
+                    // ->whereNotNull('sex_offender_status')
+                    // ->whereNotNull('national_status')
+                    // ->whereNotNull('national_status')
+                    // ->get();
+                    // if($users){
+                    //     \Log::info("Total Users to check for report ". count($users));
+                    // }
+                    // else{
+                    //     \Log::info("No Users ");
+                    // }
+                    
+        foreach($users as $user){
+            $this->checkReport($user);
+        }
+
+
         // return 0;
     }
+
+
+    private function checkReport($user){
+        $userid = $user->userid;
+        // $user = User::where('userid', $userid)->first();
+
+        $rep = new ReportController();
+        if($user->chekrreportid != NULL){
+            // return "report id not null";
+            if($user->ssn_trace == 'complete' && $user->national_status == 'complete' && $user->sex_offender_status == 'clear' && $user->chekrstatus == 'clear'){
+                // get no need to get report
+                // User::where('userid', $userid)->update(['accountstatus'=> 'Approved']);
+
+                $user->accountstatus = AccountStatus::Approved;
+                $user->save();
+                \Log::info("Cron: User already created report and status is clear". $user->userid);
+                // return $user;
+            }
+            else{
+                \Log::info("Cron: User already created report and status is not clear". $user->userid);
+                $cont = new Controller();
+                $report = $cont->getchekrreportFromServer($user);
+                // return response()->json(['status' => "1",
+                //         'message'=> 'Report created',
+                //         'data' => $report, 
+                // ]);
+            }
+            // return "getting report details";
+            
+        }
+        else{
+            $id = $this->createCandidate($user);
+            $report = $rep->getCheckrReport($id);
+            $report_error = null;
+            // return $report;
+            if(!isset($report->error)){
+                \Log::info("Cron: User created newly " . $user->userid);
+                $id = $report->id;
+                User::where('userid', $user->userid)->update(['chekrreportid' => $id]);
+                $user = User::where('userid', $user->userid)->first();
+                // return response()->json(['status' => "1",
+                //     'message'=> 'Report created',
+                //     'data' => new UserProfileFullResource($user), 
+                // ]);
+            }
+            else{
+                $report_error = $report->error . " " . $user->userid;
+                \Log::info("Cron: ". $report_error);
+                // return response()->json(['status' => "0",
+                //     'message'=> $report_error,
+                //     'data' => null, 
+                // ]);
+            }
+        }
+    }
+
+
+    public function createCheckrCandidate($data){
+        $data["copy_requested"] = true;
+        $data['no_middle_name'] = true;
+        $api_key = env('chekrapikey');
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, 'https://api.checkr.com/v1/candidates');
+        curl_setopt($curl, CURLOPT_USERPWD, $api_key . ":");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        
+        $response = curl_exec($curl);
+        $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        
+        curl_close($curl);
+        
+        $json = json_decode($response, true);
+        return $json;
+        if(array_key_exists('id', $json)){
+            return $json['id'];
+        }
+        return NULL;
+    }
+
+
+    private function createCandidate($user){
+            if($user->chekrcandidateid == NULL){
+                            $dob = '';
+                            
+                            if($user->dob){
+                                // return ["date" => "". $user->dob];
+                                $dob = Carbon::createFromFormat('m/d/Y', $user->dob)->format('Y-m-d');
+                                
+                                $data = [
+                                    "first_name" => $user->name,
+                                    "last_name" => $user->name,
+                                    "phone" => $user->phone,
+                                    "email" => $user->email,
+                                    "dob" => $dob,
+                                    "ssn" => $user->ssn,
+                                    "zipcode"=>$user->zip,
+                                ];
+                                
+                                $json = $this->createCheckrCandidate($data);
+                                
+                                if(array_key_exists('id', $json)){
+                                    $user->chekrcandidateid = $json['id'];
+                                    
+                                    User::where('userid', $user->userid)->update(['chekrcandidateid' => $json["id"]]);
+                                    return $json['id'];
+    
+                                }
+                                else{
+                                    $chekr_error = $json['error'];
+                                    return NULL;
+                                }
+                            }
+                            else{
+                                
+                            }
+            }
+            else{
+                return $user->chekrcandidateid;
+            }
+        }
 }
---
+++ cron.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/home/u549595587/public_html/braver/artisan schedule:run >> /dev/null 2>&1
