d15a7f79f7bd458a72d580e037314e6644a6186d
E8-Labs <nahomnega23@gmail.com>
2022-08-09 18:22:39 +0500

    Notifications table added

--- database/migrations/2022_08_09_130006_create_notification_types_table.php
+++ database/migrations/2022_08_09_130006_create_notification_types_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Model\NotificationTypes;
+use App\Models\NotificationTypes;
 
 return new class extends Migration
 {
