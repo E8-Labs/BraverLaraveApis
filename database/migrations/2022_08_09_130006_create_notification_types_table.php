b1f6570d5f1e62246af2a6308cbd8986be469d1f
E8-Labs <nahomnega23@gmail.com>
2022-08-09 18:18:05 +0500

    Notifications table added

--- database/migrations/2022_08_09_130006_create_notification_types_table.php
+++ database/migrations/2022_08_09_130006_create_notification_types_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use app\Model\NotificationTypes;
 
 return new class extends Migration
 {
@@ -19,11 +20,11 @@ return new class extends Migration
             $table->timestamps();
         });
         \DB::table('notification_types')->insert([
-            ['id'=> NotificationType::TypeMessage, 'name' => 'New Message'],
-            ['id'=> NotificationType::TypeReservation, 'name' => 'New Reservation'],
-            ['id'=> NotificationType::InvoicePaid, 'name' => 'Invoice Paid'],
-            ['id'=> NotificationType::NewUser, 'name' => 'New User'],
-            ['id'=> NotificationType::AdminBroadcast, 'name' => 'Admin Broadcast'],
+            ['id'=> NotificationTypes::TypeMessage, 'name' => 'New Message'],
+            ['id'=> NotificationTypes::TypeReservation, 'name' => 'New Reservation'],
+            ['id'=> NotificationTypes::InvoicePaid, 'name' => 'Invoice Paid'],
+            ['id'=> NotificationTypes::NewUser, 'name' => 'New User'],
+            ['id'=> NotificationTypes::AdminBroadcast, 'name' => 'Admin Broadcast'],
             
         ]);
     }
